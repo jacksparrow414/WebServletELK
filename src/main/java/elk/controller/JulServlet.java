4ad8cbb45b35809fcb2ade1e40072dcd5ae1d340
Serendipity <jacksparrow414@163.com>
2021-06-05 11:50:35 +0800

    adjust加入Log4jBridgeHandler，在不改动现有代码的前提下，将jul输出到log4j中

--- src/main/java/elk/controller/JulServlet.java
+++ src/main/java/elk/controller/JulServlet.java
@@ -1,17 +1,19 @@
 package elk.controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
+/**
+ * <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/logging/overview.html#a1.0">JD8 JUL文档</a>
+ */
 public class JulServlet extends HttpServlet {
 
-    private static final Logger LOGGER = Logger.getLogger(JulServlet.class.getName());
+    private static final Logger LOGGER = Logger.getLogger("my.jul.logger");
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -20,10 +22,15 @@ public class JulServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        LogManager.getLogManager().readConfiguration(new FileInputStream("classpath:/logging.properties"));
         LOGGER.fine("this is debug log");
         LOGGER.info("this is info log");
         LOGGER.warning("this is warn log");
         LOGGER.severe("this is error log");
+        try {
+            int a = 1/0;
+        }catch (ArithmeticException exception) {
+            LOGGER.log(Level.SEVERE, "这是完整打印错误堆栈日志", exception);
+        }
+        resp.getWriter().write("{\"msg\":\"ok\"}");
     }
 }
