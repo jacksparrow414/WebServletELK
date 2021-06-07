aeb2b738e6ce4002fce5cb376a7996c2a608f448
DESKTOP-F6RHSU9\etix-2021-05 <hongbo.du@etix.com>
2021-06-07 16:13:55 +0800

    init and destroy Filter

--- src/main/java/elk/config/MdcFilter.java
+++ src/main/java/elk/config/MdcFilter.java
@@ -1,13 +1,10 @@
 package elk.config;
 
+import org.apache.logging.log4j.ThreadContext;
+
+import javax.servlet.*;
 import java.io.IOException;
 import java.util.UUID;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import org.apache.logging.log4j.ThreadContext;
 
 /**
  * <a href="https://logging.apache.org/log4j/2.x/manual/thread-context.html">MDC文档</a>
@@ -15,6 +12,14 @@ import org.apache.logging.log4j.ThreadContext;
  * @date 2021/5/30
  */
 public class MdcFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
     @Override
     public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
         ThreadContext.put("id", UUID.randomUUID().toString());
